f2f1f021877a008fd348e717d59a8580f8814f21
DEV <devnull@example.com>
2026-05-22 21:01:16 +0100

    build modife!

--- backend/build.sh
+++ backend/build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -o errexit
 
-# Upgrade pip
 pip install --upgrade pip
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run migrations
+python manage.py check --deploy
 python manage.py migrate
