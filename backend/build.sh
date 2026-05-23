6dc4859600facd6cce7316aecd079abdd2da5b3e
DEV <devnull@example.com>
2026-05-23 12:38:08 +0100

    update vercel

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py check --deploy
+python manage.py makemigrations
 python manage.py migrate
 python manage.py create_admin
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
 
     # ==============================
